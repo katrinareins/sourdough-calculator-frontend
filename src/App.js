67096d033cd5fb1a9828b3812bbeb82915c60a75
learn-co-kat <twothistles@gmail.com>
2020-01-30 13:31:38 -0800

    Added links to nav bar, state to App.js

--- src/App.js
+++ src/App.js
@@ -5,22 +5,37 @@ import loginScreen from './login_page/loginScreen';
 import addNotesScreen from './addNotes_page/addNotesScreen';
 import newBakeScreen from './newBake_page/newBakeScreen';
 import viewBakesScreen from './viewBakes_page/viewBakesScreen';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route path='/' exact component={loginScreen} />
-          <Route path='/newBake' component={newBakeScreen} />
-          <Route path='/addnotes' component={addNotesScreen} />
-          <Route path='/viewbakes' component={viewBakesScreen} />
-        </Switch>
-      </div>
-    </Router>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       loggedIn: false,
+       email: '',
+       password: '',
+       bakes: []
+    }
+  }
+  
+  
+  render() {
+    return (
+      <Router>
+        <div>
+          <Nav />
+          <Switch>
+            <Route path='/' exact component={loginScreen} />
+            <Route path='/newBake' component={newBakeScreen} />
+            <Route path='/addnotes' component={addNotesScreen} />
+            <Route path='/viewbakes' component={viewBakesScreen} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/other/Nav.js
+++ src/other/Nav.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import '../App.css'
-
+import { Link } from 'react-router-dom'
 
 function Nav() {
     return (
         <div>
             <nav className='nav'>
                 <h3>Absolute Baking Success</h3>
-                <ul>
-                    <li>Create new bake</li>
-                    <li>View my bakes</li>
+                <ul className='nav-links'>
+                    <Link to='/newBake'>
+                        <li>Create new bake</li>
+                    </Link>
+                    <Link to='/viewbakes'>
+                        <li>View my bakes</li>
+                    </Link>
                 </ul>
             </nav>
         </div>
