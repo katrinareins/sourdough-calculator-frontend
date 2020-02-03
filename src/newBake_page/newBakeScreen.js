0b254643f66d260bf0ce8cfcc97d2468f73c4a1a
learn-co-kat <twothistles@gmail.com>
2020-02-03 09:21:28 -0800

    Form elements added to new bake page.

--- src/newBake_page/newBakeScreen.js
+++ src/newBake_page/newBakeScreen.js
@@ -5,6 +5,28 @@ export class NewBakeScreen extends Component {
         return (
             <div>
                 <h1>new bake screen</h1>
+                <h3>Quantities are calculated based on a basic recipe that calls for 20% leaven and 2% salt. Adjust the Baker's Percentage to see the required quantity of each ingredient. </h3>
+                <p>
+                Novice bakers - begin with lower hydration doughs (between 70-75%).
+                </p>
+                <form>
+                    <label>Name your bake</label>
+                    <input type='text' name='bakeName'></input>
+                    <p>Hydration percentage desired</p>
+                    <input type='text' name='hydrationPerc'></input>
+                    <p>Ingredients</p>
+                    <p>Weight(g)</p>
+                    <p>Baker's Percentage</p>
+                    <label>Total Flour</label>
+                    <input type='text' placeholder='Enter desired total flour' name='totalFlour'></input>
+                    <p>100%</p>
+                    <label>Water</label>
+                    <input type='text' name='totalWater'></input>
+                    <label>Leaven</label>
+                    <input type='text' name='totalLeaven'></input>
+                    <label>Salt</label>
+                    <input type='text' name='totalSalt'></input>
+                </form>
             </div>
         )
     }
