2f2f21016a021b64f664f30704c6b8f26513210a
learn-co-kat <twothistles@gmail.com>
2020-02-04 14:40:24 -0800

    Log in directs to view bakes.

--- src/login_page/loginScreen.js
+++ src/login_page/loginScreen.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 export class LoginScreen extends Component {
 
@@ -43,9 +44,11 @@ export class LoginScreen extends Component {
               onChange={(e) => this.handleInputChange(e)}
             />      
 
+            <Link to='/viewbakes'>            
             <button type="submit" onClick={this.handleClick} >
                 Login
-              </button>
+            </button>
+            </Link>
         </form> 
       </div>
     )
@@ -53,90 +56,3 @@ export class LoginScreen extends Component {
 }
 
 export default LoginScreen
-
-
-
-
-// export default function LoginScreen(props) {
-
-//   const onSubmit = (values) => {
-//     props.handleSubmitProps(values)
-//   }
-
-//   return (
-//     <div>
-//       <form>
-
-//       </form>
-//     </div>
-//   )
-// }
-
-
-{/* <Formik
-  initialValues={{ email: "", password: "" }}
-  
-  onSubmit={values => handleSubmitProps(values)}
-  
-  validationSchema={Yup.object().shape({
-    email: Yup.string()
-    .email()
-    .required("Required"),
-    password: Yup.string()
-    .required("No password provided.")
-  })}
-  >
-    {props => {
-      const {
-        values,
-        touched,
-        errors,
-        isSubmitting,
-        handleChange,
-        handleBlur,
-        handleSubmit
-      } = props;
-
-      return (
-        <div>
-          <form onSubmit={handleSubmit}>
-            <label htmlFor="email">Email</label>
-            <input
-              name="email"
-              type="text"
-              placeholder="Enter your email"
-              value={values.email}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              className={errors.email && touched.email && "error"}
-            />
-            {errors.email && touched.email && (
-              <div className="input-feedback">{errors.email}</div>
-            )}
-            <label htmlFor="email">Password</label>
-            <input
-              name="password"
-              type="password"
-              placeholder="Enter your password"
-              value={values.password}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              className={errors.password && touched.password && "error"}
-            />
-            {errors.password && touched.password && (
-              <div className="input-feedback">{errors.password}</div>
-            )}
-
-            <button type="submit" disabled={isSubmitting} >
-                Login
-              </button>
-
-          </form>
-              <Link to='/viewbakes'>
-                <button>Go to my bakes!</button>
-              </Link>
-
-        </div>
-      );
-    }}
-  </Formik> */}
