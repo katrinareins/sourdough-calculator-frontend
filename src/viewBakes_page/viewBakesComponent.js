36a1f2eb601d558a91ca817328ed4350846f2d34
learn-co-kat <twothistles@gmail.com>
2020-02-04 09:31:29 -0800

    Baker's bakes are rendering in view my bakes.

--- src/viewBakes_page/viewBakesComponent.js
+++ src/viewBakes_page/viewBakesComponent.js
@@ -1,11 +1,22 @@
 import React from 'react'
 
 export default function ViewBakesComponent(props) {
+    const { name, rating, hydration, total_flour_g, total_flour_p, water_g, water_p, salt_g, salt_p, leaven_g, leaven_p } = props.bake
+
     return (
         <div>
-        {/* const { name } = props; */}
-            <p>Is this working</p>
-            <p>{props.bake.name}</p>
+
+            <h3>{name}</h3>
+            <p>Hydration: {hydration}</p>
+            <p>Rating: {rating}</p>
+            <p>Total flour (g): {total_flour_g}</p>
+            <p>Total flour (p): {total_flour_p}</p>
+            <p>Water (g): {water_g}</p>
+            <p>Water (p): {water_p}</p>
+            <p>Salt (g): {salt_g}</p>
+            <p>Salut (p): {salt_p}</p>
+            <p>Leaven (g): {leaven_g}</p>
+            <p>Leaven (p): {leaven_p}</p>
         </div>
     )
 }
