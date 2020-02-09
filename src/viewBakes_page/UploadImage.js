08f6971e9065db39bb230753f8f48e6922c5700c
learn-co-kat <twothistles@gmail.com>
2020-02-08 18:58:20 -0800

    upload image feature

--- src/viewBakes_page/UploadImage.js
+++ src/viewBakes_page/UploadImage.js
@@ -2,24 +2,21 @@ import React, { Component } from 'react'
 import S3FileUpload from 'react-s3';
 
 export class uploadImage extends Component {
-
+    
     constructor(props) {
         super(props)
-    
+        
         this.state = {
             selectedFile: null
         }
-
-        this.config = {
-            bucketName: 'sourdoughimageuploader',
-            // dirName: 'Enter Folder Name ', /* optional */
-            region: 'US West (Oregon)',
-            accessKeyId: 'AKIAIXAR63Q25JRDH6IQ',
-            secretAccessKey: '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'
-        };
+        
+        
+        // header("Access-Control-Allow-Origin: *");
+        // header("Access-Control-Allow-Methods: POST");
+        // header("Access-Control-Allow-Headers: Origin, Methods, Content-Type");
     }
-
-
+    
+    
     fileSelectedHandler = (event) => {
         console.log('photo selected', event.target.files[0])
         this.setState({
@@ -27,9 +24,22 @@ export class uploadImage extends Component {
         })
     }
     
-    fileUploadHandler = (selectedFile, config) => {
+    fileUploadHandler = () => {
+        const config = {
+            bucketName: 'sourdoughimageuploader',
+            // dirName: 'Enter Folder Name ', /* optional */
+            region: 'US West (Oregon)',
+            // region: 'us-west-2',
+            accessKeyId: 'AKIAIXAR63Q25JRDH6IQ',
+            secretAccessKey: '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',
+        //             header: "Access-Control-Allow-Origin: *",
+        // header: "Access-Control-Allow-Methods: POST",
+        // header: "Access-Control-Allow-Headers: Origin, Methods, Content-Type"
+        };
+        console.log('is this working???')
+        console.log('selected file: ', this.state.selectedFile)
         console.log(config)
-        S3FileUpload.uploadFile(selectedFile, this.config)
+        S3FileUpload.uploadFile(this.state.selectedFile, config)
         .then(data => {
             console.log(data.location)
         })
@@ -58,3 +68,19 @@ export class uploadImage extends Component {
 }
 
 export default uploadImage
+
+
+// <?xml version="1.0" encoding="UTF-8"?>
+// <CORSConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+// <CORSRule>
+//     <AllowedOrigin>*</AllowedOrigin>
+//     <AllowedMethod>PUT</AllowedMethod>
+//     <AllowedMethod>POST</AllowedMethod>
+//     <AllowedMethod>DELETE</AllowedMethod>
+//     <AllowedHeader>*</AllowedHeader>
+// </CORSRule>
+// <CORSRule>
+//     <AllowedOrigin>*</AllowedOrigin>
+//     <AllowedMethod>GET</AllowedMethod>
+// </CORSRule>
+// </CORSConfiguration>
