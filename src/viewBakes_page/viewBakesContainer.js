fd30e7d1d64158f65428db796cbc9257f36138e8
learn-co-kat <twothistles@gmail.com>
2020-02-10 17:46:23 -0800

    More on sorting.

--- src/viewBakes_page/viewBakesContainer.js
+++ src/viewBakes_page/viewBakesContainer.js
@@ -6,29 +6,37 @@ export class ViewBakesContainer extends Component {
     // for sorting --> take in param. If the param is nothing, map cards as normal. 
     // if the param is date - check each bake and sort by date
     // if the param is ranking - check each bake and sort by ranking
-    
-    mapThroughBakes = () => {
-        return this.props.bakes.map((bake, index) => {
-            return <ViewBakesComponent 
-            bake={bake} 
-            key={index} 
-            renderNotes={this.renderNotes} 
-            handleDelete={this.props.handleDelete} 
-            handleNotePost={this.props.handleNotePost} 
-            deleteNote={this.props.deleteNote}
-            notePatchRequest={this.props.notePatchRequest}
-            handleRatingPatch={this.props.handleRatingPatch}
-            />
-        } )
+
+    mapThroughBakes = (selection) => {
+        if (selection === 'none'){
+            return this.props.bakes.map((bake, index) => {
+                return <ViewBakesComponent 
+                bake={bake} 
+                key={index} 
+                renderNotes={this.renderNotes} 
+                handleDelete={this.props.handleDelete} 
+                handleNotePost={this.props.handleNotePost} 
+                deleteNote={this.props.deleteNote}
+                notePatchRequest={this.props.notePatchRequest}
+                handleRatingPatch={this.props.handleRatingPatch}
+                />
+            } )
+
+        } else if (selection === 'date'){
+
+        } else if (selection === 'ranking'){
+            
+        }
+
     }
 
     handleFilter = (event) => {
         console.log('filter selection', event.target.value)
         let value = event.target.value
         if(value === 'date'){
-            
+            this.mapThroughBakes('date')
         }else if (value === 'ranking'){
-
+            this.mapThroughBakes('ranking')
         }
     }
 
@@ -46,7 +54,7 @@ export class ViewBakesContainer extends Component {
                 </div>
 
                 <div className='main-container-view-bakes'>
-                    {this.mapThroughBakes()} 
+                    {this.mapThroughBakes('none')} 
                 </div>
 
             </div>
