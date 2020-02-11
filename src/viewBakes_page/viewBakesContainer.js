d1fa502acb71e6da6772529fe2825fcd6dfe7433
learn-co-kat <twothistles@gmail.com>
2020-02-10 22:36:36 -0800

    Filter works and cards get sorted.

--- src/viewBakes_page/viewBakesContainer.js
+++ src/viewBakes_page/viewBakesContainer.js
@@ -3,47 +3,32 @@ import React, { Component } from 'react'
 
 export class ViewBakesContainer extends Component { 
 
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             allBakes: [],
+             filter: ''
+        }
+    }
+
     // for sorting --> take in param. If the param is nothing, map cards as normal. 
     // if the param is date - check each bake and sort by date
     // if the param is rating - check each bake and sort by rating
 
-    mapThroughBakes = (selection) => {
-        if (selection === 'none'){
-            // map over sorted array
-            return this.props.bakes.map((bake, index) => {
-                return <ViewBakesComponent 
-                bake={bake} 
-                key={index} 
-                renderNotes={this.renderNotes} 
-                handleDelete={this.props.handleDelete} 
-                handleNotePost={this.props.handleNotePost} 
-                deleteNote={this.props.deleteNote}
-                notePatchRequest={this.props.notePatchRequest}
-                handleRatingPatch={this.props.handleRatingPatch}
-                />
-            } )
-
-        } else if (selection === 'date'){
+    setCardState = (selection) => {
+        console.log('setCardState called')
+        if (selection === 'date'){
             console.log('prompted to sort by date! These are the bake props:', this.props.bakes)
 
             const sortedArrayDate = this.props.bakes.slice().sort(function(a, b){
                 return new Date(b.date) - new Date(a.date);
             })
             console.log('SORTED ARRAY BY DATE', sortedArrayDate)
-
-            return sortedArrayDate.map((bake, index) => {
-                return <ViewBakesComponent 
-                bake={bake} 
-                key={index} 
-                renderNotes={this.renderNotes} 
-                handleDelete={this.props.handleDelete} 
-                handleNotePost={this.props.handleNotePost} 
-                deleteNote={this.props.deleteNote}
-                notePatchRequest={this.props.notePatchRequest}
-                handleRatingPatch={this.props.handleRatingPatch}
-                />
-            } )
-
+            this.setState({
+                allBakes: sortedArrayDate,
+                filter: 'date'
+            })
         } else if (selection === 'rating'){
             console.log('prompted to sort by rating! These are the bake props:', this.props.bakes)
             const sortedArrayRating = this.props.bakes.slice().sort(function(a, b) {
@@ -56,31 +41,42 @@ export class ViewBakesContainer extends Component {
                 }
             });
             console.log('SORTED ARRAY BY RATING', sortedArrayRating)
-
-            return sortedArrayRating.map((bake, index) => {
-                return <ViewBakesComponent 
-                bake={bake} 
-                key={index} 
-                renderNotes={this.renderNotes} 
-                handleDelete={this.props.handleDelete} 
-                handleNotePost={this.props.handleNotePost} 
-                deleteNote={this.props.deleteNote}
-                notePatchRequest={this.props.notePatchRequest}
-                handleRatingPatch={this.props.handleRatingPatch}
-                />
-            } )
+            this.setState({
+                allBakes: sortedArrayRating,
+                filter: 'rating'
+            })
+        } else if (selection === ''){
+            this.setState({
+                allBakes: this.props.bakes,
+                filter: ''
+            })
+            console.log('state on null', this.state)
         }
+    }
 
+    renderCards = () => {
+        console.log('RENDER CARDS FUNCTION CALLED')
+
+        return this.state.allBakes.map((bake, index) => {
+            return <ViewBakesComponent 
+            bake={bake} 
+            key={index} 
+            renderNotes={this.renderNotes} 
+            handleDelete={this.props.handleDelete} 
+            handleNotePost={this.props.handleNotePost} 
+            deleteNote={this.props.deleteNote}
+            notePatchRequest={this.props.notePatchRequest}
+            handleRatingPatch={this.props.handleRatingPatch}
+            />
+        } )
+    }
+
+    componentDidMount(){
+        this.setCardState('')
     }
 
     handleFilter = (event) => {
-        console.log('filter selection', event.target.value)
-        let value = event.target.value
-        if(value === 'date'){
-            this.mapThroughBakes('date')
-        }else if (value === 'rating'){
-            this.mapThroughBakes('rating')
-        }
+        this.setCardState(event.target.value)
     }
 
     render() {
@@ -97,7 +93,7 @@ export class ViewBakesContainer extends Component {
                 </div>
 
                 <div className='main-container-view-bakes'>
-                    {this.mapThroughBakes('none')} 
+                    {this.renderCards()} 
                 </div>
 
             </div>
